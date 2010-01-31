5ef393754a6bfba8ea5c89efa39f52cdc09e5927
Sarah Vessels <cheshire137@gmail.com>
2010-01-30 21:52:46 -0500

    Updating index page to list friends (though it just seems to show user IDs) from example Facebook API code.  Updating setup_database to catch the exception where the objects already exist.  Should figure out nicer way to only initialize the Facebook data store objects once.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'config.php';
 
 $facebook = new Facebook($api_key, $secret);
 $facebook->require_frame();
-$user = $facebook->require_login();
+$user_id = $facebook->require_login();
 
 try
 {
@@ -20,6 +20,19 @@ catch (Exception $ex)
 {
   echo 'Failed to setup database: ' . $ex;
 }
+
+// Greet the currently logged-in user!
+echo "<p>Hello, <fb:name uid=\"$user_id\" useyou=\"false\" />!</p>";
+
+// Print out at most 25 of the logged-in user's friends,
+// using the friends.get API method
+echo "<p>Friends:</p>";
+$friends = $facebook->api_client->friends_get();
+$friends = array_slice($friends, 0, 25);
+echo '<ul>';
+foreach ($friends as $friend) {
+  echo "<li>$friend</li>";
+}
+echo "</ul>\n";
 ?>
-<h2>Hi <fb:name firstnameonly="true" uid="<?=$user?>" useyou="false"/>!</h2><br/>
 <a href="<?= $facebook->get_add_url() ?>">Put Github in your profile</a>, if you haven't already!
--- lib.php
+++ lib.php
@@ -1,30 +1,20 @@
 <?php
-require_once 'wmsfacebook/WMSFacebook.php';
+require_once 'config.php';
+require_once 'facebook.php';
 
 function setup_database($facebook)
 {
-  $users_table = 'users';
-  $repos_table = 'repositories';
-  $facebook->api_client->data_createObjectType($users_table);
-  $facebook->api_client->data_createObjectType($repos_table);
-  $facebook->api_client->data_defineObjectProperty($users_table, 'user_id', 2);
-  $facebook->api_client->data_defineObjectProperty($users_table, 'password', 2);
-  $facebook->api_client->data_defineObjectProperty($repos_table, 'name', 2);
-  $facebook->api_client->data_defineObjectProperty($repos_table, 'user_id', 2);
-  /*$dataStore = $facebook->getWMSFacebookDataStore();
+  global $users_table, $repos_table;
   
-  $usersTable = $dataStore->createObjectType('users');
-  if (null != $usersTable && get_class($usersTable) == "WMSFacebookDataStoreObject")
+  try
   {
-    $usersTable.createPropertyString("user_id");
-    $usersTable.createPropertyString("password");
+    $facebook->api_client->data_createObjectType($users_table);
+    $facebook->api_client->data_createObjectType($repos_table);
+    $facebook->api_client->data_defineObjectProperty($users_table, 'user_id', 2);
+    $facebook->api_client->data_defineObjectProperty($users_table, 'password', 2);
+    $facebook->api_client->data_defineObjectProperty($repos_table, 'name', 2);
+    $facebook->api_client->data_defineObjectProperty($repos_table, 'user_id', 2);
   }
-  
-  $repositoriesTable = $dataStore->createObjectType('repositories');
-  if (null != $repositoriesTable && get_class($repositoriesTable) == "WMSFacebookDataStoreObject")
-  {
-    $repositoriesTable.createPropertyString("name");
-    $repositoriesTable.createPropertyString("user_id");
-  }*/
+  catch (FacebookRestClientException $ex) { }
 }
 ?>
