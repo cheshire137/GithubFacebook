166df376dfe74ef5346fea002b0bfca3b6c36348
Sarah Vessels <cheshire137@gmail.com>
2010-02-02 18:30:38 -0500

    Adding requirement for curb Curl gem.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
@@ -20,8 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'composite_primary_keys'
-  config.gem 'facebooker'
+  #config.gem 'facebooker'
   #config.gem 'rfacebook'
+  config.gem 'curb'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
