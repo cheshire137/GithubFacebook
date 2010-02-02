f22131c36ae46d69ae436220f38069e675ebb6c8
Sarah Vessels <cheshire137@gmail.com>
2010-02-02 10:33:05 -0500

    Removing old manual PHP stuff on master branch.  Will be used for Rails.

--- footer.inc.php
+++
@@ -1,9 +0,0 @@
-<?php require_once 'config.inc.php'; ?>
-    <script type="text/javascript">
-    function initFB() {
-        FB_RequireFeatures(["XFBML"], function(){
-            FB.init("<?php echo $api_key; ?>", "xd_receiver.htm");
-        });}
-    </script>
-    </body>
-</html>
