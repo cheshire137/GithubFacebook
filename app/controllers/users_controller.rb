f2c441c183a93fab655eca218c1aa40a7d1bf712
Sarah Vessels <cheshire137@gmail.com>
2010-02-02 13:04:57 -0500

    Trying to fix form bug.  Fleshing out create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
       flash[:error] = 'Please use the form provided'
       redirect_to :action => 'index' and return
     end
+    
+    unless params[:user]
+      flash[:error] = 'No data given'
+      redirect_to :action => 'index' and return
+    end
+    
+    if User.create(params[:user])
+      flash[:notice] = 'Successfully added Github name'
+      redirect_to :action => 'index'
+    else
+      flash[:error] = 'Could not add Github name'
+      @users = User.find_all_by_facebook_id @facebook_id
+      @user = User.new(params[:user])
+      render :template => 'index'
+    end
   end
   
   def index
