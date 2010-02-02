453a78c06a9fd3e8067abd5fe6817b9d090651a8
Sarah <moneypenny@wasp.dreamhost.com>
2010-02-02 15:40:40 -0800

    Including GitHub API gem.

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   #config.gem 'facebooker'
   #config.gem 'rfacebook'
   config.gem 'curb'
+  config.gem 'ruby-github'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
