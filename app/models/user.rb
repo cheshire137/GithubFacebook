f0c10b8abcdc58089b56c94a3f1a598522e7fc34
Sarah <moneypenny@wasp.dreamhost.com>
2010-02-02 16:12:50 -0800

    Fixing typo in GitHub::API usage.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   end
   
   def repositories
-    GitHub::Api.user(github_name).repositories
+    GitHub::API.user(github_name).repositories
   rescue
     []
   end
