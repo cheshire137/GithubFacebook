d56d4b17e08fcd91f580432c231706d993ec6d48
Sarah Vessels <cheshire137@gmail.com>
2010-02-02 19:01:41 -0500

    Moving Github model's contents into User.  Displaying repos stuff on index.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ActiveRecord::Base
   set_primary_keys :facebook_id, :github_name
   validates_presence_of :facebook_id, :github_name
-  belongs_to :github, :foreign_key => 'github_name'
+  
+  def User.valid_github?(user_name)
+    return false if user_name.nil? || user_name.empty?
+    GitHub::API.user(user_name)
+    true
+  rescue OpenURI::HTTPError
+    false
+  end
+  
+  def repositories
+    GitHub::Api.user(github_name).repositories
+  rescue
+    []
+  end
   
   def validate
-    unless Github.user_exists?(github_name)
+    unless User.valid_github?(github_name)
       errors.add(:github_name, 'is not a registered Github user')
     end
   end
